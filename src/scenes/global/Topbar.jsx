12965d79076ce394fa50ab3db547574164d60ae9
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-06-08 19:20:15 +0530

    Searchbar update

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,31 @@
+import {Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchdIcon from "@mui/icons-material/Search";
+
+
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+
+    return 
+    <Box display= "flex" justifyContent="space-between" p={2}> 
+          {/* SearchBar */}
+          <Box display="flex">
+
+          </Box>
+
+            <IconButton></IconButton>
+    </Box>;
+
+};
 
 export default Topbar;
