ce0f44a0b20c248a12676283fe03d7022273b06f
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-06-08 19:35:26 +0530

    Searchcss

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -24,7 +24,7 @@ const Topbar = () => {
           backgroundColor={colors.primary[400]} 
           borderRadius="3px">
          <InputBase sx={{ml: 2, flex: 1}} placeholder="Search" />
-         <IconButton>
+         <IconButton type="button" sx={{ p:1}}>
             <SearchdIcon />
          </IconButton>
           </Box>
