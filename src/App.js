8cd0f468e2b86a33a7285fd0d64fb9429084e754
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-06-09 01:26:46 +0530

    theme and style updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
-
-import { ColorModeContext, useMode } from './theme';
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/global/dashboard";
-//import Team from "./scenes/global/team";
-//import Invoices from "./scenes/global/invoices";
-//import Contacts from "./scenes/global/contacts";
-//import Bar from "./scenes/global/bar";
-//import Form from "./scenes/global/form";
-//import Line from "./scenes/global/line";
-//import Pie from "./scenes/global/pie";
-//import FAQ from "./scenes/global/faq";
-//import Geography from "./scenes/global/geography";
-//import Calender from "./scenes/global/calender";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
 function App() {
-  const [theme, colorMode] =useMode();
-  
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-    <div className="app">
-      <Sidebar />
-      <main className="content">
-        <Topbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          {/* <Route path="/team" element={<Team />} /> */}
-        {/*  <Route path="/contacts" element={<Contacts />} /> */}
-        {/*  <Route path="/invoices" element={<Invoices />} /> */}
-        {/* <Route path="/form" element={<Form />} /> */}
-        {/*  <Route path="/pie" element={<Pie />} /> */}
-        {/*  <Route path="/line" element={<Line />} /> */}
-        {/*  <Route path="/faq" element={<FAQ />} /> */}
-        {/*  <Route path="/geography" element={<Geography />} /> */}
-        {/*  <Route path="/calender" element={<Calender />} /> */}
-        </Routes>
-      </main>
-    </div>
-    </ThemeProvider>
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
 export default App;
