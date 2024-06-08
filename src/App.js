244cba7812d221627993c3dfde98e6a12fe28de7
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-06-08 17:49:51 +0530

    imported the setup into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
+  const [theme, colorMode] =useMode();
+  
   return (
-    <div className="app">
-     
-    </div>
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+    <div className="app"></div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
 
 export default App;
